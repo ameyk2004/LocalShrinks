0131c3b6f77c55fe993ab8f41292df41b1110918
ameyk2004 <kulkarniamey2004@gmail.com>
2024-03-03 17:35:18 +0530

    Added a Login Page

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Login Screen", style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
